41364ce31534f538cf1afc657e923dbeb618fd49
paper209 <fleshed-stroll84@icloud.com>
2026-04-13 13:56:11 +0900

    Refactor TCP session management and hooks

--- main.c
+++ main.c
@@ -6,126 +6,22 @@
 #include <linux/net_namespace.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
-#include <linux/spinlock.h>
+
+#include "tcp/tcp.h"
 
 // [\x00\x00\x00\x01][id length (8 bits)][id (n bits)]
-
-#define TRUE 1
-#define FALSE 0
-
-struct tcp_session {
-    __be32 saddr;
-    __be16 sport;
-    __be16 dport;
-
-    __be32 init_seq;
-
-    char *buffer;
-    int buffer_len;
-};
-
-
-struct tcp_session **tcp_sessions = NULL;
-int tcp_sessions_len = 0;
-
-spinlock_t tcp_lock;
-
-void deinit_tcp_sessions(void) {
-    for (int i = 0; i < tcp_sessions_len; i++) {
-        struct tcp_session *session = tcp_sessions[i];
-        
-        kfree(session->buffer);
-        kfree(session);
-    }
-    
-    kfree(tcp_sessions);
-}
-
-int add_tcp_session(struct iphdr *iph, struct tcphdr *tcph) {
-    spin_lock(&tcp_lock);
-
-    struct tcp_session *session = kmalloc(sizeof(struct tcp_session), GFP_ATOMIC);
-    if (!session) {
-        spin_unlock(&tcp_lock);
-        return FALSE;
-    }
-
-    session->saddr = iph->saddr;
-    session->sport = tcph->source;
-    session->dport = tcph->dest;
-    session->init_seq = tcph->seq;
-
-    session->buffer = NULL;
-    session->buffer_len = 0;
-
-    tcp_sessions = krealloc(tcp_sessions, sizeof(*tcp_sessions)*(++tcp_sessions_len), GFP_ATOMIC);
-    if (!tcp_sessions) {
-        kfree(session);
-        kfree(tcp_sessions);
-        
-        spin_unlock(&tcp_lock);
-
-        return FALSE;
-    }
-
-    tcp_sessions[tcp_sessions_len-1] = session;
-    spin_unlock(&tcp_lock);
-
-    return TRUE;
-}
-
-struct tcp_session *get_tcp_session(struct iphdr *iph, struct tcphdr *tcph) {
-    for (int i = 0; i < tcp_sessions_len; i++) {
-        struct tcp_session *session = tcp_sessions[i];
-        if (session->saddr == iph->saddr && session->sport == tcph->source && session->dport == tcph->dest) {
-            return session;
-        }
-    }
-
-    return NULL;
-}
-
-int add_tcp_data(struct iphdr *iph, struct tcphdr *tcph) {
-    spin_lock(&tcp_lock);
-    
-    struct tcp_session *session = get_tcp_session(iph, tcph);
-    if (!session) {
-        spin_unlock(&tcp_lock);
-        return FALSE;
-    }
-
-    char *data = (char *)tcph + tcph->doff * 4;
-    int data_len = ntohs(iph->tot_len)-((iph->ihl*4)+(tcph->doff*4));
-    if (data_len <= 0) {
-        spin_unlock(&tcp_lock);
-        return TRUE;
-    }
-
-    int offset = ntohl(tcph->seq) - ntohl(session->init_seq);
-    if (offset+data_len > session->buffer_len) {
-        session->buffer = krealloc(session->buffer, offset+data_len, GFP_ATOMIC);
-        if (!session->buffer) {
-            spin_unlock(&tcp_lock);
-
-            return FALSE;
-        }
-
-        session->buffer_len = offset+data_len;
-    }
-
-    memcpy(session->buffer+offset, data, data_len);
-    spin_unlock(&tcp_lock);
-
-    return TRUE;
-}
-
-unsigned int hook_tcp(void *pb, struct sk_buff *skb, const struct nf_hook_state *state) {
+unsigned int hook(void *pb, struct sk_buff *skb, const struct nf_hook_state *state) {
     const struct iphdr *iph = ip_hdr(skb);
     if (iph->protocol == 6) {
         const struct tcphdr *tcph = tcp_hdr(skb);
         if (tcph->syn && !tcph->ack) {
             if (!add_tcp_session(iph, tcph)) {
                 printk(KERN_ERR "add tcp session error!!\n");
+                return NF_ACCEPT;
+            }
+        } else if (tcph->fin) {
+            if (!remove_tcp_session(iph, tcph)) {
+                printk(KERN_ERR "remove tcp session error!!\n");
                 return NF_ACCEPT;
             }
         } else {
@@ -141,13 +37,13 @@ unsigned int hook_tcp(void *pb, struct sk_buff *skb, const struct nf_hook_state 
 
 const struct nf_hook_ops nfho = {
     .pf = PF_INET,
-    .hook = hook_tcp,
+    .hook = hook,
     .hooknum = NF_INET_LOCAL_OUT,
     .priority = NF_IP_PRI_FIRST,
 };
 
 int init(void) {
-    spin_lock_init(&tcp_lock);
+    init_tcp_lock();
     nf_register_net_hook(&init_net, &nfho);
     printk(KERN_INFO "i'm loaded!!!\n");
 
