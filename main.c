0d8c445badeaae265665418f95d3dfcf9c1cf94f
paper209 <fleshed-stroll84@icloud.com>
2026-04-13 13:12:16 +0900

    Add spinlock for thread safety in TCP session handling

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <linux/net_namespace.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <linux/spinlock.h>
 
 // [\x00\x00\x00\x01][id length (8 bits)][id (n bits)]
 
@@ -23,10 +24,13 @@ struct tcp_session {
     int buffer_len;
 };
 
+
 struct tcp_session **tcp_sessions = NULL;
 int tcp_sessions_len = 0;
 
-void deinit_tcp_sessions() {
+spinlock_t tcp_lock;
+
+void deinit_tcp_sessions(void) {
     for (int i = 0; i < tcp_sessions_len; i++) {
         struct tcp_session *session = tcp_sessions[i];
         
@@ -38,8 +42,13 @@ void deinit_tcp_sessions() {
 }
 
 int add_tcp_session(struct iphdr *iph, struct tcphdr *tcph) {
+    spin_lock(&tcp_lock);
+
     struct tcp_session *session = kmalloc(sizeof(struct tcp_session), GFP_ATOMIC);
-    if (!session) return FALSE;
+    if (!session) {
+        spin_unlock(&tcp_lock);
+        return FALSE;
+    }
 
     session->saddr = iph->saddr;
     session->sport = tcph->source;
@@ -53,11 +62,14 @@ int add_tcp_session(struct iphdr *iph, struct tcphdr *tcph) {
     if (!tcp_sessions) {
         kfree(session);
         kfree(tcp_sessions);
+        
+        spin_unlock(&tcp_lock);
 
         return FALSE;
     }
 
     tcp_sessions[tcp_sessions_len-1] = session;
+    spin_unlock(&tcp_lock);
 
     return TRUE;
 }
@@ -73,23 +85,36 @@ struct tcp_session *get_tcp_session(struct iphdr *iph, struct tcphdr *tcph) {
     return NULL;
 }
 
-int add_data(struct iphdr *iph, struct tcphdr *tcph) {
+int add_tcp_data(struct iphdr *iph, struct tcphdr *tcph) {
+    spin_lock(&tcp_lock);
+    
     struct tcp_session *session = get_tcp_session(iph, tcph);
-    if (!session) return FALSE;
+    if (!session) {
+        spin_unlock(&tcp_lock);
+        return FALSE;
+    }
 
     char *data = (char *)tcph + tcph->doff * 4;
     int data_len = ntohs(iph->tot_len)-((iph->ihl*4)+(tcph->doff*4));
-    if (data_len <= 0) return TRUE;
+    if (data_len <= 0) {
+        spin_unlock(&tcp_lock);
+        return TRUE;
+    }
 
     int offset = ntohl(tcph->seq) - ntohl(session->init_seq);
     if (offset+data_len > session->buffer_len) {
         session->buffer = krealloc(session->buffer, offset+data_len, GFP_ATOMIC);
-        if (!session->buffer) return FALSE;
+        if (!session->buffer) {
+            spin_unlock(&tcp_lock);
+
+            return FALSE;
+        }
 
         session->buffer_len = offset+data_len;
     }
 
     memcpy(session->buffer+offset, data, data_len);
+    spin_unlock(&tcp_lock);
 
     return TRUE;
 }
@@ -104,8 +129,8 @@ unsigned int hook_tcp(void *pb, struct sk_buff *skb, const struct nf_hook_state 
                 return NF_ACCEPT;
             }
         } else {
-            if (!add_data(iph, tcph)) {
-                printk(KERN_ERR "add data error!!\n");
+            if (!add_tcp_data(iph, tcph)) {
+                printk(KERN_ERR "add tcp data error!!\n");
                 return NF_ACCEPT;
             }
         }
@@ -122,6 +147,7 @@ const struct nf_hook_ops nfho = {
 };
 
 int init(void) {
+    spin_lock_init(&tcp_lock);
     nf_register_net_hook(&init_net, &nfho);
     printk(KERN_INFO "i'm loaded!!!\n");
 
