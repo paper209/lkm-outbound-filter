d3c34a3db08075921310df057506120140e258d4
paper209 <fleshed-stroll84@icloud.com>
2026-04-15 08:29:09 +0900

    Define error codes for TCP session management
    Added error codes for TCP operations.

--- tcp/tcp.h
+++ tcp/tcp.h
@@ -5,6 +5,12 @@
 #include <linux/tcp.h>
 #include <linux/skbuff.h>
 
+#define TCP_ALLOC_ERROR -1
+#define TCP_REALLOC_ERROR -2
+#define TCP_SESSION_NOT_FOUND -3
+#define TCP_INVALID_LENGTH -4
+#define TCP_BUFFER_COPY_ERROR -5
+
 void init_tcp_lock(void);
 void deinit_tcp_sessions(void);
 int remove_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
