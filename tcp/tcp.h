43eeaa74987827616a4abd326c8bb0df3b76e6a2
paper209 <fleshed-stroll84@icloud.com>
2026-04-15 05:44:50 +0900

    Modify add_tcp_data function signature to use sk_buff

--- tcp/tcp.h
+++ tcp/tcp.h
@@ -3,11 +3,12 @@
 
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <linux/skbuff.h>
 
 void init_tcp_lock(void);
 void deinit_tcp_sessions(void);
 int remove_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
 int add_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
-int add_tcp_data(struct iphdr *iph, struct tcphdr *tcph);
+int add_tcp_data(struct sk_buff *skb, struct iphdr *iph, struct tcphdr *tcph);
 
 #endif
