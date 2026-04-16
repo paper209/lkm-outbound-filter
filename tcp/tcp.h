8261c53e2833061e416c48c2605848fd226b8dc0
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 07:18:29 +0900

    Change TCP error codes to enum for better organization

--- tcp/tcp.h
+++ tcp/tcp.h
@@ -5,11 +5,13 @@
 #include <linux/tcp.h>
 #include <linux/skbuff.h>
 
-#define TCP_ALLOC_ERROR -1
-#define TCP_REALLOC_ERROR -2
-#define TCP_SESSION_NOT_FOUND -3
-#define TCP_INVALID_LENGTH -4
-#define TCP_BUFFER_COPY_ERROR -5
+enum {
+    TCP_ALLOC_ERROR = -1,
+    TCP_REALLOC_ERROR = -2,
+    TCP_SESSION_NOT_FOUND = -3,
+    TCP_INVALID_LENGTH = -4,
+    TCP_BUFFER_COPY_ERROR = -5,
+};
 
 void init_tcp_lock(void);
 void deinit_tcp_sessions(void);
