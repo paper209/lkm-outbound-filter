b2f08ce5942a7b3381759a1bf9066df16ab98c62
paper209 <fleshed-stroll84@icloud.com>
2026-04-16 10:06:05 +0900

    Add port filtering functionality
    Implement functions to manage blocked ports in the filter.

---
+++ filter/filter_port.c
@@ -0,0 +1,64 @@
+#include <linux/kernel.h>
+#include <linux/types.h>
+
+#include "filter.h"
+
+__be16 *block_ports = NULL;
+unsigned int block_ports_len = 0;
+
+bool is_block_port(__be16 port) {
+    spin_lock(&filter_lock);
+    for (int i = 0; i < block_ports_len; i++) {
+        if (block_ports[i] == port) {
+            spin_unlock(&filter_lock);
+            return true;
+        } 
+    }
+    spin_unlock(&filter_lock);
+
+    return false;
+}
+
+int add_filter_port(__be16 port) {
+    spin_lock(&filter_lock);
+
+    __be16 *ports = krealloc(block_ports, sizeof(__be16)*++block_ports_len, GFP_ATOMIC);
+    if (!ports) {
+        block_ports_len--;
+        spin_unlock(&filter_lock);
+
+        return FILTER_REALLOC_ERROR;
+    }
+    block_ports = ports;
+    
+    block_ports[block_ports_len-1] = port;
+    spin_unlock(&filter_lock);
+    
+    return 0;
+}
+
+int remove_filter_port(__be16 port) {
+    spin_lock(&filter_lock);
+
+    for (int i = 0; i < block_ports_len; i++) {
+        if (block_ports[i] == port) {
+            block_ports_len--;
+            for (int j = i; j < block_ports_len; j++) {
+                block_ports[j] = block_ports[j+1];
+            }
+
+            __be16 *ports = krealloc(block_ports, sizeof(__be16)*block_ports_len, GFP_ATOMIC);
+            if (!ports) {
+                spin_unlock(&filter_lock);
+
+                return FILTER_REALLOC_ERROR;
+            }
+            block_ports = ports;
+
+            break;
+        }
+    }
+
+    spin_unlock(&filter_lock);
+    return 0;
+}
