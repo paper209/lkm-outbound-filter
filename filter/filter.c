231d825c7429e353c3e10d4d189d212f5840f463
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 09:34:19 +0900

    Refactor filter.c by removing unused code
    Removed unnecessary includes and the is_block function.

--- filter/filter.c
+++ filter/filter.c
@@ -1,8 +1,5 @@
 #include <linux/kernel.h>
-#include <linux/types.h>
-#include <linux/skbuff.h>
-#include <linux/ip.h>
-#include <linux/tcp.h>
+#include <linux/spinlock.h>
 
 #include "filter.h"
 
@@ -10,19 +7,4 @@ spinlock_t filter_lock;
 
 void init_filter_lock(void) {
     spin_lock_init(&filter_lock);
-}
-
-bool is_block(struct iphdr *iph, struct sk_buff *skb) {
-    switch (iph->protocol) {
-        case 6: {
-            struct tcphdr *tcph = tcp_hdr(skb);
-            if (is_block_port(tcph->dest)) {
-                return true;
-            }
-
-            break;
-        }
-    }
-
-    return false;
 }
