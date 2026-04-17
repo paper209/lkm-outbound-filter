65545e176d401dd30e10885034e1355e25779d1e
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 12:02:18 +0900

    Add deinit_block_address function declaration

--- filter/filter_address.h
+++ filter/filter_address.h
@@ -4,6 +4,8 @@
 #include <linux/ip.h>
 #include <linux/types.h>
 
+void deinit_block_address(void);
+
 bool is_block_address(struct iphdr *iph);
 int add_address_filter(__be32 address);
 int remove_address_filter(__be32 address);
