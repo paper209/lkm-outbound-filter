ae5e3d1ba8f8b6ae34ba2233d2a2a50ce0d81dca
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 12:02:43 +0900

    Add deinit_block_ports function declaration
    Added a function declaration for deinit_block_ports.

--- filter/filter_port.h
+++ filter/filter_port.h
@@ -3,6 +3,8 @@
 
 #include <linux/types.h>
 
+void deinit_block_ports(void);
+
 bool is_block_port(struct iphdr *iph, struct sk_buff *skb);
 int add_port_filter(__be16 port);
 int remove_port_filter(__be16 port);
