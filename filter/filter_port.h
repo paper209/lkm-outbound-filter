fdffb3f4f17a9e826448d00999635d018e2b5908
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 11:40:42 +0900

    Rename filter port functions for clarity

--- filter/filter_port.h
+++ filter/filter_port.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 
 bool is_block_port(struct iphdr *iph, struct sk_buff *skb);
-int add_filter_port(__be16 port);
-int remove_filter_port(__be16 port);
+int add_port_filter(__be16 port);
+int remove_port_filter(__be16 port);
 
 #endif
