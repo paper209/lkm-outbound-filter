54305cad531a8421e8bc4e49ab32fd7cb95c5468
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 09:34:30 +0900

    Update filter.h to include spinlock and remove is_block
    Removed is_block function declaration and included spinlock header.

--- filter/filter.h
+++ filter/filter.h
@@ -1,7 +1,7 @@
 #ifndef FILTER_H
 #define FILTER_H
 
-#include <linux/skbuff.h>
+#include "linux/spinlock.h"
 
 #include "filter_port.h"
 
@@ -12,6 +12,5 @@ enum {
 extern spinlock_t filter_lock;
 
 void init_filter_lock(void);
-bool is_block(struct iphdr *iph, struct sk_buff *skb);
 
 #endif
