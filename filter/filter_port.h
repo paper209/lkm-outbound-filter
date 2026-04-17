7dde9a6c97500e0cfa428d2f0ca7078af6b10832
paper209 <fleshed-stroll84@icloud.com>
2026-04-17 09:34:59 +0900

    Modify is_block_port to accept iphdr and sk_buff

--- filter/filter_port.h
+++ filter/filter_port.h
@@ -3,7 +3,7 @@
 
 #include <linux/types.h>
 
-bool is_block_port(__be16 port);
+bool is_block_port(struct iphdr *iph, struct sk_buff *skb);
 int add_filter_port(__be16 port);
 int remove_filter_port(__be16 port);
 
