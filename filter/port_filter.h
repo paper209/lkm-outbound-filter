cc619f7cf5b83919ffba90f243bce2b7fce7e8fd
paper209 <fleshed-stroll84@icloud.com>
2026-04-19 10:53:31 +0900

    Refactor port filter functions in port_filter.h

--- filter/port_filter.h
+++ filter/port_filter.h
@@ -1,11 +1,11 @@
 #ifndef FILTERPORT_H
 #define FILTERPORT_H
 
+#include <linux/ip.h>
 #include <linux/types.h>
 
-void deinit_block_ports(void);
-
-bool is_block_port(struct iphdr *iph, struct sk_buff *skb);
+void deinit_port_filter(void);
+bool port_filter(struct iphdr *iph, struct sk_buff *skb);
 int add_port_filter(__be16 port);
 int remove_port_filter(__be16 port);
 
