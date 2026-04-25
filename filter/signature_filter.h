ff28fd2c7c76830ccc549e641b8fdc0ef841036b
paper209 <fleshed-stroll84@icloud.com>
2026-04-25 12:45:51 +0900

    Add signature_filter header file with function declarations

---
+++ filter/signature_filter.h
@@ -0,0 +1,9 @@
+#ifndef SIGNATURE_FILTER_H
+#define SIGNATURE_FILTER_H
+
+void deinit_signature_filter(void);
+int add_signature_filter(char *signature, unsigned int signature_len);
+int remove_signature_filter(char *signature, unsigned int signature_len);
+bool signature_filter(struct iphdr *iph, struct sk_buff *skb);
+
+#endif
