eb16e9b13a62b05bb762a8e3c4a7822d28fbf81b
paper209 <fleshed-stroll84@icloud.com>
2026-04-27 06:59:21 +0900

    Update tcp.h

--- tcp/tcp.h
+++ tcp/tcp.h
@@ -29,6 +29,6 @@ void deinit_tcp_sessions(void);
 int remove_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
 int add_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
 int add_tcp_data(struct sk_buff *skb, struct iphdr *iph, struct tcphdr *tcph);
-struct tcp_session *get_tcp_session(struct iphdr *iph, struct tcphdr *tcph);
+char *get_tcp_buffer(struct iphdr *iph, struct tcphdr *tcph, unsigned int *len);
 
 #endif
