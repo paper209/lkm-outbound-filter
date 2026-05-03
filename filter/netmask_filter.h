96d97d7d3bdb2609f115b09205f5f3ae20dab8cc
paper209 <fleshed-stroll84@icloud.com>
2026-05-04 03:04:14 +0900

    Add init_netmask_lock function declaration

--- filter/netmask_filter.h
+++ filter/netmask_filter.h
@@ -4,6 +4,7 @@
 #include <linux/ip.h>
 #include <linux/types.h>
 
+void init_netmask_lock(void);
 void deinit_netmask_filter(void);
 bool netmask_filter(struct iphdr *iph);
 int add_netmask_filter(__be32 address, __be32 mask);
