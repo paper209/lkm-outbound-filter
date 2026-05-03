24027c48ed7e38481b3cfc801e4aa9141df6845e
paper209 <fleshed-stroll84@icloud.com>
2026-05-04 03:03:36 +0900

    Add spinlock for port filter management

--- filter/port_filter.c
+++ filter/port_filter.c
@@ -4,6 +4,7 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <linux/spinlock.h>
 
 #include "filter.h"
 
@@ -12,13 +13,19 @@ struct filter {
     __be16 port;
 };
 
+spinlock_t port_lock;
+
 struct filter *port_filters = NULL;
 unsigned int port_filters_len = 0;
 
+void init_port_lock(void) {
+    spin_lock_init(&port_lock);
+}
+
 void deinit_port_filter(void) {
-    spin_lock(&filter_lock);
+    spin_lock(&port_lock);
     kfree(port_filters);
-    spin_unlock(&filter_lock);
+    spin_unlock(&port_lock);
 }
 
 bool port_filter(struct iphdr *iph, struct sk_buff *skb) {
@@ -45,26 +52,26 @@ bool port_filter(struct iphdr *iph, struct sk_buff *skb) {
         default: return false;
     }
     
-    spin_lock(&filter_lock);
+    spin_lock(&port_lock);
     for (int i = 0; i < port_filters_len; i++) {
         struct filter *filter = &port_filters[i];
         if (filter->protocol == packet.protocol && filter->port == packet.port) {
-            spin_unlock(&filter_lock);
+            spin_unlock(&port_lock);
             return true;
         } 
     }
-    spin_unlock(&filter_lock);
+    spin_unlock(&port_lock);
 
     return false;
 }
 
 int add_port_filter(__be16 port, __u8 protocol) {
-    spin_lock(&filter_lock);
+    spin_lock(&port_lock);
 
     struct filter *filters = krealloc(port_filters, sizeof(struct filter)*++port_filters_len, GFP_ATOMIC);
     if (!filters) {
         port_filters_len--;
-        spin_unlock(&filter_lock);
+        spin_unlock(&port_lock);
 
         return FILTER_REALLOC_ERROR;
     }
@@ -76,13 +83,13 @@ int add_port_filter(__be16 port, __u8 protocol) {
     };
     printk(KERN_INFO "added port filter: %d\n", ntohs(port));
     
-    spin_unlock(&filter_lock);
+    spin_unlock(&port_lock);
     
     return 0;
 }
 
 int remove_port_filter(__be16 port, __u8 protocol) {
-    spin_lock(&filter_lock);
+    spin_lock(&port_lock);
 
     for (int i = 0; i < port_filters_len; i++) {
         struct filter *filter = &port_filters[i];
@@ -94,7 +101,7 @@ int remove_port_filter(__be16 port, __u8 protocol) {
 
             struct filter *filters = krealloc(port_filters, sizeof(struct filter)*port_filters_len, GFP_ATOMIC);
             if (!filters) {
-                spin_unlock(&filter_lock);
+                spin_unlock(&port_lock);
 
                 return FILTER_REALLOC_ERROR;
             }
@@ -105,6 +112,6 @@ int remove_port_filter(__be16 port, __u8 protocol) {
     }
     printk(KERN_INFO "removed port filter: %d\n", ntohs(port));
     
-    spin_unlock(&filter_lock);
+    spin_unlock(&port_lock);
     return 0;
 }
