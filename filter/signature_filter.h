91985661169b4a67a055d8314f8748bc0041e926
paper209 <fleshed-stroll84@icloud.com>
2026-05-04 03:03:25 +0900

    Add init_signature_lock function declaration

--- filter/signature_filter.h
+++ filter/signature_filter.h
@@ -1,6 +1,7 @@
 #ifndef SIGNATURE_FILTER_H
 #define SIGNATURE_FILTER_H
 
+void init_signature_lock(void);
 void deinit_signature_filter(void);
 int add_signature_filter(char *signature, unsigned int signature_len);
 int remove_signature_filter(char *signature, unsigned int signature_len);
