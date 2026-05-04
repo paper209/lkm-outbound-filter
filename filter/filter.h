6eb714787c9e25407c4a251f53c62b2d35cf51eb
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 08:38:46 +0900

    Add filter state enum and error code for full filter

--- filter/filter.h
+++ filter/filter.h
@@ -5,12 +5,21 @@
 #include "netmask_filter.h"
 #include "signature_filter.h"
 
+// error code
 enum {
     FILTER_REALLOC_ERROR = -1,
     FILTER_ALLOC_ERROR = -2,
+    FILTER_IS_FULL = -3,
+};
+
+// filter's state
+enum filter_state {
+    FILTER_USED = 1,
+    FILTER_EMPTY = 0,
 };
 
 void deinit_filter(void);
+int init_filters(unsigned int max_len);
 void init_filters_lock(void);
 
 #endif
