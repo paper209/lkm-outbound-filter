5b07cd48ed6a0d5a2d54a79bbc990bab9328908f
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 08:38:33 +0900

    Implement init_filters and update deinit_filter
    Added init_filters function to initialize filters and updated deinit_filter function.

--- filter/filter.c
+++ filter/filter.c
@@ -2,12 +2,23 @@
 
 #include "filter.h"
 
+// init filters
+int init_filters(unsigned int max_len) {
+    // init port filter
+    int n = init_port_filter(max_len);
+    if (n < 0) return n;
+
+    return 0;
+}
+
+// to be removed
 void init_filters_lock(void) {
-    init_port_lock();
+    //init_port_lock();
     init_netmask_lock();
     init_signature_lock();
 }
 
+// to be updated
 void deinit_filter(void) {
     deinit_port_filter();
     deinit_netmask_filter();
