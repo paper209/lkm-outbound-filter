23aae050eb33dc3a848d57b44d9ac79482e16085
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 08:39:03 +0900

    Refactor port filter to use static allocation
    Refactor port filter implementation to use a static array and improve memory management. Replace dynamic allocation with static allocation for better performance and reliability.

--- filter/port_filter.c
+++ filter/port_filter.c
@@ -8,26 +8,110 @@
 
 #include "filter.h"
 
+// port filter
 struct filter {
     __u8 protocol;
     __be16 port;
+
+    enum filter_state state;
 };
 
-spinlock_t port_lock;
+static spinlock_t port_lock;
 
-struct filter *port_filters = NULL;
-unsigned int port_filters_len = 0;
+static struct filter *filters = NULL; // port filters array
+static unsigned int max_filters_len = 0; // max port filters length
 
-void init_port_lock(void) {
+// init port spinlock
+static void init_port_lock(void) {
     spin_lock_init(&port_lock);
 }
 
+// init port filters array and spinlock
+int init_port_filter(unsigned int len) {
+    init_port_lock();    
+    spin_lock(&port_lock);
+
+    max_filters_len = len; // set max filters length
+    filters = kcalloc(max_filters_len, sizeof(struct filter), GFP_ATOMIC);
+    if (!filters) {
+        spin_unlock(&port_lock);
+        return FILTER_ALLOC_ERROR;
+    }
+
+    spin_unlock(&port_lock);
+    return 0;
+}
+
+// deinit port filters array
 void deinit_port_filter(void) {
     spin_lock(&port_lock);
-    kfree(port_filters);
+    kfree(filters);
     spin_unlock(&port_lock);
 }
 
+// find free index on filters array
+static int find_free_index(__be16 port) {
+    // calculate minimum start index number
+    unsigned int min = ntohs(port)%max_filters_len;
+    
+    // find free index number and return
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->state == FILTER_EMPTY) {
+            return i;
+        }
+    }
+
+    // find free index number failed
+    return FILTER_IS_FULL;
+}
+
+// add new port filter
+int new_port_filter(__be16 port, __u8 protocol) {
+    spin_lock(&port_lock);
+
+    // find a free index
+    int i = find_free_index(port);
+    if (i < 0) {
+        spin_unlock(&port_lock);
+        return i;
+    }
+
+    // add filters array
+    filters[i] = (struct filter) {
+        .port = port,
+        .protocol = protocol,
+        .state = FILTER_USED,
+    };
+
+    // debug
+    printk(KERN_INFO "added port filter: %d\n", ntohs(port));
+
+    spin_unlock(&port_lock);
+    return 0;
+}
+
+// remove port filter to filters's array
+void remove_port_filter(__be16 port, __u8 protocol) {
+    spin_lock(&port_lock);
+    
+    // calculate minimum start index number
+    unsigned int min = ntohs(port)%max_filters_len;
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->protocol == protocol && f->port == port) {
+            memset(f, 0, sizeof(struct filter));
+            break;
+        }
+    }
+
+    // debug
+    printk(KERN_INFO "removed port filter: %d\n", ntohs(port));
+
+    spin_unlock(&port_lock);
+}
+
+// check the port filters
 bool port_filter(struct iphdr *iph, struct sk_buff *skb) {
     struct filter packet;
     switch (iph->protocol) {
@@ -52,10 +136,14 @@ bool port_filter(struct iphdr *iph, struct sk_buff *skb) {
         default: return false;
     }
     
+    // calculate minimum start index number
+    unsigned int min = ntohs(packet.port)%max_filters_len;
     spin_lock(&port_lock);
-    for (int i = 0; i < port_filters_len; i++) {
-        struct filter *filter = &port_filters[i];
-        if (filter->protocol == packet.protocol && filter->port == packet.port) {
+
+    // check the port filters
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->protocol == packet.protocol && f->port == packet.port) {
             spin_unlock(&port_lock);
             return true;
         } 
@@ -63,55 +151,4 @@ bool port_filter(struct iphdr *iph, struct sk_buff *skb) {
     spin_unlock(&port_lock);
 
     return false;
-}
-
-int add_port_filter(__be16 port, __u8 protocol) {
-    spin_lock(&port_lock);
-
-    struct filter *filters = krealloc(port_filters, sizeof(struct filter)*++port_filters_len, GFP_ATOMIC);
-    if (!filters) {
-        port_filters_len--;
-        spin_unlock(&port_lock);
-
-        return FILTER_REALLOC_ERROR;
-    }
-    port_filters = filters;
-
-    port_filters[port_filters_len-1] = (struct filter){
-        .protocol = protocol,
-        .port = port,
-    };
-    printk(KERN_INFO "added port filter: %d\n", ntohs(port));
-    
-    spin_unlock(&port_lock);
-    
-    return 0;
-}
-
-int remove_port_filter(__be16 port, __u8 protocol) {
-    spin_lock(&port_lock);
-
-    for (int i = 0; i < port_filters_len; i++) {
-        struct filter *filter = &port_filters[i];
-        if (filter->port == port && filter->protocol == protocol) {
-            port_filters_len--;
-            for (int j = i; j < port_filters_len; j++) {
-                port_filters[j] = port_filters[j+1];
-            }
-
-            struct filter *filters = krealloc(port_filters, sizeof(struct filter)*port_filters_len, GFP_ATOMIC);
-            if (!filters) {
-                spin_unlock(&port_lock);
-
-                return FILTER_REALLOC_ERROR;
-            }
-            port_filters = filters;
-
-            break;
-        }
-    }
-    printk(KERN_INFO "removed port filter: %d\n", ntohs(port));
-    
-    spin_unlock(&port_lock);
-    return 0;
 }
