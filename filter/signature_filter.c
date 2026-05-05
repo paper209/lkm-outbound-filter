ec029c05b33b58b293c55912a3387b7bd0c0cabc
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 15:18:30 +0900

    Refactor signature filter management and locking

--- filter/signature_filter.c
+++ filter/signature_filter.c
@@ -9,105 +9,138 @@
 #include "filter.h"
 #include "../tcp/tcp.h"
 
+// signature filter
 struct filter {
     char *signature;
     unsigned int signature_len;
+
+    enum filter_state state;
 };
 
-spinlock_t signature_lock;
+static spinlock_t signature_lock;
 
-struct filter *signature_filters = NULL;
-unsigned int signature_filters_len = 0;
+static struct filter *filters = NULL; // signature filters array
+static unsigned int max_filters_len = 0; // signature filters array's max length
 
-void init_signature_lock(void) {
+// init signature filter's spinlock
+static void init_signature_lock(void) {
     spin_lock_init(&signature_lock);
 }
 
-void deinit_signature_filter(void) {
+// init signature filters array and spinlock
+int init_signature_filter(unsigned int max_len) {
+    init_signature_lock();
     spin_lock(&signature_lock);
-    for (int i = 0; i < signature_filters_len; i++) {
-        struct filter *f = &signature_filters[i];
-        kfree(f->signature);
+
+    max_filters_len = max_len;
+    filters = kcalloc(max_filters_len, sizeof(struct filter), GFP_ATOMIC);
+    if (!filters) {
+        spin_unlock(&signature_lock);
+        return FILTER_ALLOC_ERROR;
     }
 
-    kfree(signature_filters);
+    spin_unlock(&signature_lock);
+    return 0;
+}
+
+// deinit signature filters array and signature array
+void deinit_signature_filter(void) {
+    spin_lock(&signature_lock);
+    for (int i = 0; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        kfree(f->signature);
+    }
+    kfree(filters);
     spin_unlock(&signature_lock);
 }
 
-int add_signature_filter(char *signature, unsigned int signature_len) {
-    spin_lock(&signature_lock);
-
-    struct filter *filters = krealloc(signature_filters, sizeof(struct filter)*++signature_filters_len, GFP_ATOMIC);
-    if (!filters) {
-        signature_filters_len--;
-        spin_unlock(&signature_lock);
-
-        return FILTER_REALLOC_ERROR;
+// find free index on netmask filters array
+static int find_free_index(char *signature) {
+    // calculate minimum start index number
+    unsigned int min = signature[0]%max_filters_len;
+    
+    // find free index number and return
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->state == FILTER_EMPTY) {
+            return i;
+        }
     }
-    signature_filters = filters;
 
+    // find free index number failed
+    return FILTER_IS_FULL;
+}
+
+// add a new signature filter
+int new_signature_filter(char *signature, unsigned int signature_len) {
+    spin_lock(&signature_lock);
+    
+    // find free index using signature len
+    int i = find_free_index(signature);
+    if (i < 0) {
+        spin_unlock(&signature_lock);
+        return i;
+    }
+
+    // copy a signature buffer
     char *copy_signature = kmalloc(signature_len, GFP_ATOMIC);
     if (!copy_signature) {
-        signature_filters_len--;
         spin_unlock(&signature_lock);
         return FILTER_ALLOC_ERROR;
     }
     memcpy(copy_signature, signature, signature_len);
 
-    signature_filters[signature_filters_len-1] = (struct filter){
+    // add signature filter
+    filters[i] = (struct filter){
         .signature = copy_signature,
         .signature_len = signature_len,
+        .state = FILTER_USED,
     };
 
+    // debug
     printk(KERN_INFO "added signature filter: %u\n", signature_len);
-    
+
     spin_unlock(&signature_lock);
-    
     return 0;
 }
 
-int remove_signature_filter(char *signature, unsigned int signature_len) {
+// remove signature filter
+void remove_signature_filter(char *signature, unsigned int signature_len) {
     spin_lock(&signature_lock);
 
-    for (int i = 0; i < signature_filters_len; i++) {
-        struct filter *f = &signature_filters[i];
+    // calculate minimum start index number
+    unsigned int min = signature[0]%max_filters_len;
+    for (int i = min; i < max_filters_len; i ++) {
+        struct filter *f = &filters[i];
         if (f->signature_len == signature_len && memcmp(signature, f->signature, f->signature_len) == 0) {
             kfree(f->signature);
-            signature_filters_len--;
-
-            for (int j = i; j < signature_filters_len; j++) {
-                signature_filters[j] = signature_filters[j+1];
-            }
-
-            struct filter *filters = krealloc(signature_filters, sizeof(struct filter)*signature_filters_len, GFP_ATOMIC);
-            if (!filters) {
-                spin_unlock(&signature_lock);
-
-                return FILTER_REALLOC_ERROR;
-            }
-            signature_filters = filters;
-
+            memset(f, 0, sizeof(struct filter));
             break;
         }
     }
+
+    // debug
     printk(KERN_INFO "removed signature filter: %u\n", signature_len);
     
     spin_unlock(&signature_lock);
-    return 0;
 }
 
-bool check_signature(const char *buf, int buf_len) {
+
+// check the signature filters
+static bool check_signature(const char *buf, int buf_len) {
     spin_lock(&signature_lock);
-    for (int i = 0; i < signature_filters_len; i++) {
-        struct filter *f = &signature_filters[i];
+
+    // calculate minimum start index number
+    unsigned int min = buf[0]%max_filters_len;
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
         if (f->signature_len > buf_len) continue;
 
+        // check the signature
         for (int j = 0; j <= buf_len-f->signature_len; j++) {
-            if (buf[j] == f->signature[0]) {
-                if (memcmp(buf+j, f->signature, f->signature_len) == 0) {
-                    spin_unlock(&signature_lock);
-                    return true;
-                }
+            if (memcmp(buf+j, f->signature, f->signature_len) == 0) {
+                spin_unlock(&signature_lock);
+                return true;
             }
         }
     }
@@ -116,6 +149,7 @@ bool check_signature(const char *buf, int buf_len) {
     return false;
 }
 
+// check the signature filters and compare the buffer
 bool signature_filter(struct iphdr *iph, struct sk_buff *skb) {
     switch (iph->protocol) {
         // tcp
