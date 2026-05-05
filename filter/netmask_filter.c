dfe6fe8770befe6ae17b531e5a9fe41673ad521a
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 10:17:56 +0900

    Refactor netmask filter implementation

--- filter/netmask_filter.c
+++ filter/netmask_filter.c
@@ -5,31 +5,118 @@
 
 #include "filter.h"
 
+// netmask filter
 struct filter {
     __be32 address;
     __be32 mask;
+
+    enum filter_state state;
 };
 
-spinlock_t netmask_lock;
+static spinlock_t netmask_lock;
 
-struct filter *netmask_filters = NULL;
-unsigned int netmask_filters_len = 0;
+static struct filter *filters = NULL; // netmask filters array
+static unsigned int max_filters_len = 0; // netmask filters max length
 
-void init_netmask_lock(void) {
+// init netmask spinlock
+static void init_netmask_lock(void) {
     spin_lock_init(&netmask_lock);
 }
 
+// init netmask filters array and spinlock
+int init_netmask_filter(unsigned int max_len) {
+    init_netmask_lock();
+    spin_lock(&netmask_lock);
+
+    max_filters_len = max_len;
+    filters = kcalloc(max_filters_len, sizeof(struct filter), GFP_ATOMIC);
+    if (!filters) {
+        spin_unlock(&netmask_lock);
+        return FILTER_ALLOC_ERROR;
+    }
+
+    spin_unlock(&netmask_lock);
+    return 0;
+}
+
+// deinit netmask filters array
 void deinit_netmask_filter(void) {
     spin_lock(&netmask_lock);
-    kfree(netmask_filters);
+    kfree(filters);
     spin_unlock(&netmask_lock);
 }
 
-bool netmask_filter(struct iphdr *iph) {
+// find free index on netmask filters array
+static int find_free_index(__be32 address) {
+    // calculate minimum start index number
+    unsigned int min = ntohl(address)%max_filters_len;
+    
+    // find free index number and return
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->state == FILTER_EMPTY) {
+            return i;
+        }
+    }
+
+    // find free index number failed
+    return FILTER_IS_FULL;
+}
+
+// add new netmask filter
+int new_netmask_filter(__be32 address, __be32 mask) {
     spin_lock(&netmask_lock);
-    for (int i = 0; i < netmask_filters_len; i++) {
-        struct filter *filter = &netmask_filters[i];
-        if ((filter->address&filter->mask) == (iph->daddr&filter->mask)) {
+    
+    // find free index
+    int i = find_free_index(address);
+    if (i < 0) {
+        spin_unlock(&netmask_lock);
+        return i;
+    }
+
+    // add filter
+    filters[i] = (struct filter) {
+        .address = address,
+        .mask = mask,
+        .state = FILTER_USED,
+    };
+
+    // debug
+    printk(KERN_INFO "added netmask filter: %pI4, %pI4\n", &address, &mask);
+
+    spin_unlock(&netmask_lock);
+    return 0;
+}
+
+// remove netmask filter
+void remove_netmask_filter(__be32 address, __be32 mask) {
+    spin_lock(&netmask_lock);
+    
+    // calculate minimum start index number
+    unsigned int min = ntohl(address)%max_filters_len;
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if (f->address == address && f->mask == mask) {
+            memset(f, 0, sizeof(struct filter));
+            break;
+        }
+    }
+
+    // debug
+    printk(KERN_INFO "removed netmask filter: %pI4, %pI4\n", &address, &mask);
+    spin_unlock(&netmask_lock);
+}
+
+// check the netmask filter
+bool netmask_filter(struct iphdr *iph) {
+    // calculate minimum start index number
+    unsigned int min = ntohl(iph->daddr)%max_filters_len;
+    spin_lock(&netmask_lock);
+
+    // check the port filters
+    for (int i = min; i < max_filters_len; i++) {
+        struct filter *f = &filters[i];
+        if ((f->address&f->mask) == (iph->daddr&f->mask)) {
             spin_unlock(&netmask_lock);
             return true;
         } 
@@ -37,57 +124,4 @@ bool netmask_filter(struct iphdr *iph) {
     spin_unlock(&netmask_lock);
 
     return false;
-}
-
-int add_netmask_filter(__be32 address, __be32 mask) {
-    spin_lock(&netmask_lock);
-
-    struct filter *filters = krealloc(netmask_filters, sizeof(struct filter)*++netmask_filters_len, GFP_ATOMIC);
-    if (!filters) {
-        netmask_filters_len--;
-        spin_unlock(&netmask_lock);
-
-        return FILTER_REALLOC_ERROR;
-    }
-    netmask_filters = filters;
-    netmask_filters[netmask_filters_len-1] = (struct filter){
-        .address = address,
-        .mask = mask,
-    };
-
-    printk(KERN_INFO "added address filter: %pI4\n", &address);
-    
-    spin_unlock(&netmask_lock);
-    
-    return 0;
-}
-
-int remove_netmask_filter(__be32 address, __be32 mask) {
-    spin_lock(&netmask_lock);
-
-    for (int i = 0; i < netmask_filters_len; i++) {
-        struct filter *filter = &netmask_filters[i];
-        if (filter->address == address && filter->mask == mask) {
-            netmask_filters_len--;
-            for (int j = i; j < netmask_filters_len; j++) {
-                netmask_filters[j] = netmask_filters[j+1];
-            }
-
-            struct filter *filters = krealloc(netmask_filters, sizeof(struct filter)*netmask_filters_len, GFP_ATOMIC);
-            if (!filters) {
-                netmask_filters_len++;
-                spin_unlock(&netmask_lock);
-
-                return FILTER_REALLOC_ERROR;
-            }
-            netmask_filters = filters;
-
-            break;
-        }
-    }
-    printk(KERN_INFO "removed netmask filter: %pI4\n", &address);
-    
-    spin_unlock(&netmask_lock);
-    
-    return 0;
 }
