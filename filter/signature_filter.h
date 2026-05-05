9c3af6d7c4f16b2b4d7152f9819a9e48ebc9c8a8
paper209 <fleshed-stroll84@icloud.com>
2026-05-05 15:18:41 +0900

    Modify signature filter functions in header file

--- filter/signature_filter.h
+++ filter/signature_filter.h
@@ -1,10 +1,12 @@
 #ifndef SIGNATURE_FILTER_H
 #define SIGNATURE_FILTER_H
 
-void init_signature_lock(void);
+int init_signature_filter(unsigned int max_len);
 void deinit_signature_filter(void);
-int add_signature_filter(char *signature, unsigned int signature_len);
-int remove_signature_filter(char *signature, unsigned int signature_len);
+
+int new_signature_filter(char *signature, unsigned int siganture_len);
+void remove_signature_filter(char *signature, unsigned int signature_len);
+
 bool signature_filter(struct iphdr *iph, struct sk_buff *skb);
 
 #endif
