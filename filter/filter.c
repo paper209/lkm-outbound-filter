672be1390dbc1d70e6fe444000b37687a6b29885
paper209 <fleshed-stroll84@icloud.com>
2026-05-20 15:23:33 +0900

    Implement fast and slow filters in filter.c
    Added fast and slow filter functions for packet filtering.

--- filter/filter.c
+++ filter/filter.c
@@ -1,4 +1,6 @@
 #include <linux/kernel.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
 
 #include "filter.h"
 
@@ -19,9 +21,31 @@ int init_filters(unsigned int max_len) {
     return 0;
 }
 
-
 void deinit_filters(void) {
     deinit_port_filter();
     deinit_netmask_filter();
     deinit_signature_filter();
+}
+
+// fast filter (port, netmask)
+bool fast_filter(struct sk_buff *skb, struct iphdr *iph) {
+    if (port_filter(iph, skb)) {
+        printk(KERN_INFO "outbound filter: fast filter: port filtered: %pI4 -> %pI4\n", &iph->saddr, &iph->daddr);
+        return true;
+    } else if (netmask_filter(iph)) {
+        printk(KERN_INFO "outbound filter: fast filter: netmask filtered: %pI4 -> %pI4\n", &iph->saddr, &iph->daddr);
+        return true;
+    }
+
+    return false;
+}
+
+// slow filter (signature filter)
+bool slow_filter(struct sk_buff *skb, struct iphdr *iph) {
+    if (signature_filter(iph, skb)) {
+        printk(KERN_INFO "outbound filter: slow filter: signature filtered: %pI4 => %pI4\n", &iph->saddr, &iph->daddr);
+        return true;
+    }
+
+    return false;
 }
