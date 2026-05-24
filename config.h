f90b75348115952cf311af3f300d95d3410dfc80
paper209 <fleshed-stroll84@icloud.com>
2026-05-25 03:29:35 +0900

    Add comments to config.h for address and timeout
    Added comments for clarity on SET_ADDR and TCP_SESSION_TIMEOUT.

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SET_ADDR 0x7F000001
+#define SET_ADDR 0x7F000001 // 127.0.0.1
 #define SET_PORT 209
 
 #define TCP_MAX_BUFFER 2048
 #define TCP_MAX_SESSIONS 1024
+#define TCP_SESSION_TIMEOUT 60 // 60 second
 
 #define FILTER_MAX_LENGTH 30
 
